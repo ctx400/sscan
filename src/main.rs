d8ee7b913ff443d642946e5661da33ad00571e34
ctx400 <43049420+ctx400@users.noreply.github.com>
2025-01-10 23:45:09 -0600

    Added a lib.rs crate for future library usage.

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,16 @@
 //! Its high level of configurability is powered by userscripts which run in
 //! an embeded [Lua](https://www.lua.org/) virtual machine.
 //!
-//! Currently, scanning is provided by the
-//! [YARA-X](https://virustotal.github.io/yara-x/) scan engine. YARA-X is a
-//! Rust implementation of the original YARA scan engine. Additional scan
-//! engines may be implemented or integrated in the future.
-//!
-//! The embedded Lua virtual machine is made possible by the
-//! [mlua](https://crates.io/crates/mlua) crate.
-//!
-
-// Modules
-mod lua;
 
 // Scope Imports
 use anyhow::Result;
 use mlua::Lua;
+use sscan::lua_api;
 
 /// Entrypoint for sscan.
 fn main() -> Result<()> {
     // Initialize the Lua virtual machine.
-    let lua: Lua = lua::init()?;
+    let lua: Lua = lua_api::init()?;
 
     // Run a test script to validate APIs
     lua.load("version() license()").exec()?;
