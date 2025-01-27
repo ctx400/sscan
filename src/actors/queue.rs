9d0db6066592a3756b0c64d95fcdab6d1e1389ab
zfcarsonb <145137624+zfcarsonb@users.noreply.github.com>
2025-01-27 10:41:10 -0600

    Docs for actors::queue::Queue

--- src/actors/queue.rs
+++ src/actors/queue.rs
@@ -23,15 +23,33 @@ use data_item::DataItem;
 use kameo::{mailbox::unbounded::UnboundedMailbox, Actor};
 use std::collections::VecDeque;
 
+/// # The Global Scan Queue
+///
+/// This actor provides a global scan queue. Userscripts can enqueue
+/// files and other items implementing trait [`DataItem`]. The queue
+/// is used for efficiently sending input items to all scan engines.
 pub struct Queue {
+    /// A double-ended queue storing items implementing [`DataItem`]
     items: VecDeque<Box<dyn DataItem>>,
 }
 
+/// # [`Queue`] is an actor.
+///
+/// This means that the queue runs on its own thread and communicates
+/// with other Rust components via message passing. This allows the
+/// queue to run alongside other asynchronous subsystems while
+/// maintaining owned mutable state without locks.
 impl Actor for Queue {
     type Mailbox = UnboundedMailbox<Self>;
 }
 
 impl Queue {
+    /// Create a new [`Queue`] of capacity `ZERO`.
+    ///
+    /// **Efficiency**: A queue of size zero will allocate very
+    /// frequently when items are enqueued. It is recommended to use
+    /// [`Queue::with_capacity()`] or [`Queue::default()`] to choose a
+    /// reasonable starting capacity.
     #[must_use]
     pub fn new() -> Self {
         Self {
@@ -39,6 +57,7 @@ impl Queue {
         }
     }
 
+    /// Create a new [`Queue`] of capacity [`usize`] data items.
     #[must_use]
     pub fn with_capacity(capacity: usize) -> Self {
         Self {
@@ -48,6 +67,7 @@ impl Queue {
 }
 
 impl Default for Queue {
+    /// Create a new [`Queue`] of capacity `2048` data items.
     fn default() -> Self {
         Self::with_capacity(2048)
     }
